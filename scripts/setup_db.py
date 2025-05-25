3c651d2f091a31d825069524167f7e40de949a37
Korir Carlos <carlkorir226@gmail.com>
2025-05-25 09:01:26 +0300

    fixes the conn import bug

--- scripts/setup_db.py
+++ scripts/setup_db.py
@@ -1,14 +1,22 @@
-from lib.db.connection import CONNECTION
-cursor = CONNECTION.cursor()
 
-with open("lib/schema.sql") as schema_file:
-    cursor.executescript(schema_file.read())
+import sqlite3
+
+connection = sqlite3.connect('lib/db/magazine.db')
+
+connection.execute("PRAGMA foreign_keys = ON;")
+
+cursor = connection.cursor()
+
+with open("lib/db/schema.sql") as schema_file:
+    schema_sql = schema_file.read()
+    cursor.executescript(schema_sql)
     print("schema.sql has been successfully executed.")
 
 with open("lib/db/seed.sql") as seed_file:
-    cursor.executescript(seed_file.read())
+    seed_sql = seed_file.read()
+    cursor.executescript(seed_sql)
     print("seed.sql has been executed succsessfully.")
 
-CONNECTION.commit()
-CONNECTION.close()
+connection.commit()
+connection.close()
 print("Magazine.db has been setup successfully.")
